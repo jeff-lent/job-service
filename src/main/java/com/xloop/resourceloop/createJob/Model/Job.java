1133f57f54a9f2eb8885fdb8898fe1e8abfc6c6e
Muhammad Waqar <mohammad.waqar@xloopdigital.com>
2023-01-20 16:30:50 +0500

    Entity Created

--- src/main/java/com/xloop/resourceloop/createJob/Model/Job.java
+++ src/main/java/com/xloop/resourceloop/createJob/Model/Job.java
@@ -1,13 +1,55 @@
 package com.xloop.resourceloop.createJob.Model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-// @Entity
-
+@Entity
 public class Job {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    // @Id
-    // private Long id;
-    // private  
+    private String description;
+    
+    //for adding department in the list.
+    private List<String> departments;
+    
+    //for adding responsibilities in the list
+    private String responsibilities;
+
+    private String designation;
+
+    private String education;
+
+    @Column(name = "experience_level")
+    private int exp_level;
+
+    //should be enum Type
+    private String emp_category;
+
+    private int vac_count;
+    
+    //should be enum Type
+    private String gender;
+
+    private List<String> skills;
+    
+    private List<String> location;
+
+    //should be enum Type 
+    private String traveling;
+
+    private List<String> benefit_perks;
+
+    //Should be Date Type
+    private String post_date;
+
+    //Should be Date Type
+    private String close_date;
 }

d4694dbf5a3b2af3d4316cc77d26fa42f27400e6
Muhammad Waqar <mohammad.waqar@xloopdigital.com>
2023-01-20 16:27:38 +0500

    initial commit for checking

---
+++ src/main/java/com/xloop/resourceloop/createJob/CreateJobApplication.java
@@ -0,0 +1,13 @@
+package com.xloop.resourceloop.createJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateJobApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/xloop/resourceloop/createJob/Model/Job.java
@@ -0,0 +1,13 @@
+package com.xloop.resourceloop.createJob.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+// @Entity
+
+public class Job {
+
+    // @Id
+    // private Long id;
+    // private  
+}
